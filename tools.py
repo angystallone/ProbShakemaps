c74f48775f8d1b769548d54e4acbdda746471928
Angela Stallone <63099520+angystallone@users.noreply.github.com>
2024-02-09 12:08:52 +0100

    Add files via upload

--- tools.py
+++ tools.py
@@ -559,7 +559,8 @@ class Main:
 
             print("********* LOADING Vs30 *******")
             # Vs30
-            vs30fullname = os.path.join(self.data_path, 'shakemap_data', 'vs30', vs30file)
+            # vs30fullname = os.path.join(self.data_path, 'shakemap_data', 'vs30', vs30file)
+            vs30fullname = os.path.join('/home/shake/shakemap_data/vs30', vs30file)
             vs30grid = GMTGrid.load(vs30fullname)
             # Interpolate Vs30 values at POIs 
             vs30_POIs = vs30grid.getValue(self.POIs_lat, self.POIs_lon, method="nearest")
